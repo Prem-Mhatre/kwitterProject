098246c0b414e9e06606d56ca9bf5c122c838f5c
Prem Mhatre <83350182+Prem-Mhatre@users.noreply.github.com>
2021-08-16 20:53:40 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -31,3 +31,30 @@ function send(){
 
     document.getElementById("msg").value = "";
 }
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+    firebase_message_id = childKey;
+    message_data = childData;
+
+    message = message_data['message'];
+    name = message_data['name'];
+    like = message_data['like'];
+
+    name_with_tag = "<h4>"+name+"</h4>";
+    message_with_tag = "<h4>" + message + "</h4>";
+    like_button = "<button id="+firebase_message_id+" value="+like+" class='btn btn-warning' onclick='updateLike(this.id)'>";
+    span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: " + like + "</span></button><hr>";
+    row = name_with_tag  + message_with_tag + like_button + span_with_tag;
+    document.getElementById("output").innerHTML += row;
+ } });  }); }
+getData();
+
+function updateLike(message_id){
+    button_id = message_id;
+    likes = document.getElementById(button_id).value;
+    update_likes = Number(likes) + 1;
+
+    firebase.database().ref(room_name).child(message_id).update({
+        like : update_likes
+    });
+}
